f1ea3856a2da860c2948ef6cd11fa4540fa7c285
Lahiru Rajakaruna Jayasinghe <140371042+LahiruJ-2003@users.noreply.github.com>
2024-02-27 14:22:04 +0530

    first commit

---
+++ src/data/todos.ts
@@ -0,0 +1,19 @@
+import { Todo } from "../types/todo";
+
+export const dummyData: Todo[] = [
+    {
+        id: 1,
+        title: "Have to Work out",
+        completed: false,
+    },
+    {
+        id: 2,
+        title: "Have to complete this code",
+        completed: false,
+    },
+    {
+        id: 3,
+        title: "Leave a nice comment",
+        completed: false,
+    },
+];
